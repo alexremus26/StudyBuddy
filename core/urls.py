a6ddfd1b944a7775d1ace81bed7d5cca88d3b80a
rosuae <rosuamdreieduard@gmail.com>
2026-03-19 10:01:17 +0200

    added node_modules to .gitignore

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='test.html')),
     path('index.html/', TemplateView.as_view(template_name='index.html')),
     path('test.html/', TemplateView.as_view(template_name='test.html'))
 ]
